de12ad5db6439d24f0d696c1bdd5669b6a649ee4
Donatien Eneman <donatien.eneman@insee.fr>
2021-02-23 17:52:15 +0100

    update doc

--- cli/get/kubeconfig.go
+++ cli/get/kubeconfig.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 
 	"github.com/fatih/color"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/viper"
 	"k8s.io/client-go/tools/clientcmd"
 	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
@@ -37,25 +38,33 @@ func ExecuteGetKubeConfig(group string, write bool, destination string) {
 	if err != nil {
 		panic(err)
 	}
+	// body, err := ioutil.ReadAll(resp.Body)
+	// fmt.Println(string(body))
 
 	onboardingResponse := new(OnboardingResponse)
 	err = json.NewDecoder(resp.Body).Decode(onboardingResponse)
 
 	config := clientcmdapi.NewConfig()
 	config.Clusters[onboardingResponse.ClusterName] = &clientcmdapi.Cluster{
-		Server: onboardingResponse.ApiserverURL,
+		Server:                onboardingResponse.ApiserverURL,
+		InsecureSkipTLSVerify: true,
+	}
+	config.AuthInfos[onboardingResponse.User] = &clientcmdapi.AuthInfo{
+		Token: onboardingResponse.Token,
 	}
 	config.AuthInfos[onboardingResponse.ClusterName] = &clientcmdapi.AuthInfo{
 		Token: onboardingResponse.Token,
 	}
 	config.Contexts[onboardingResponse.ClusterName] = &clientcmdapi.Context{
 		Cluster:   onboardingResponse.ClusterName,
-		AuthInfo:  onboardingResponse.ClusterName,
+		AuthInfo:  onboardingResponse.User,
 		Namespace: onboardingResponse.Namespace,
 	}
 	config.CurrentContext = onboardingResponse.ClusterName
 	pathOptions := clientcmd.NewDefaultPathOptions()
-
+	home, _ := homedir.Dir()
+	filePath := filepath.Join(home, ".kube", "config")
+	pathOptions.GlobalFile = filePath
 	if write {
 		if destination != "" {
 			pathOptions.GlobalFile = filepath.Join(filepath.Clean(destination), "kubeconfig")
